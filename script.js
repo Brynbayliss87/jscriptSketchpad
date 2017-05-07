89ae29b1aa38255a3238fc7cef8b6099017185df
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-07 21:10:26 +0000

    added reset button and playing around with javascript

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-  for(var x = 0; x <= 16; x++){
-    for(var y = 0; y <= 16; y++){
-      var square  = $("<div class='square'></div>");
+  var size= prompt("What size sketchpad? Enter a number between 1 and 100");
+  while (size > 1 && size < 100){
+    break;
+    alert("Invalid entry, size set to 64");
+    size=64};
+  for(var i=0;i < size*size; size++){
+    var square  = $("<div class='square'></div>");
       square.appendTo("#wrapper");
     }
-  }
 });
  
