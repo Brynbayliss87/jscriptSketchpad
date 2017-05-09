d89a529e9635ea85294622c6d9d5b44472908c29
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-09 21:13:08 +0000

    started fixing grid and css

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var size=16
+
 $(document).ready(function(){
- 
-  for(var i=0;i < size*size; size++){
+
+  for(var i=0; i < size; i++){
+    for(var j=0; j < size; j++){ 
     var square  = $("<div class='square'></div>");
-      square.appendTo("#wrapper");
+        square.appendTo("#wrapper");
     }
+  }
 });
- 
+
