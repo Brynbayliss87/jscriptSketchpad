fd7e082a79390d3c4bd578371eb7f5b172c254f6
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-09 20:30:40 +0000

    fixed some div position

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  var size= prompt("What size sketchpad? Enter a number between 1 and 100");
-  while (size > 1 && size < 100){
-    break;
-    alert("Invalid entry, size set to 64");
-    size=64};
+ 
   for(var i=0;i < size*size; size++){
     var square  = $("<div class='square'></div>");
       square.appendTo("#wrapper");
