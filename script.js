7b9fb3f5c8317ca8799314a6a343af5f67bc87f2
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-11 20:19:07 +0000

    Rounded edges and tried to make button resize grid

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-var size=16
+
 
 $(document).ready(function(){
-
+  var size = 16
+  
+  
+  
+   
+  var make_wrapper = function(size){
   for(var i=0; i < size; i++){
     for(var j=0; j < size; j++){ 
-    var square  = $("<div class='square'></div>");
-        square.appendTo("#wrapper");
+      var square  = $("<div class='square'></div>");
+      
+  
+      square.appendTo("#wrapper");
+      $("square").css("height","640/size");
+      $("square").css("height","640/size");
+        
     }
   }
+ }
+  
+  make_wrapper(size)
+
+  $("button").click(function(){
+    size = prompt("Set the size, pick a number between 1 and 64")
+    $("square").remove();
+    make_wrapper(size);
+  });
+  
+
+
 });
+
 
