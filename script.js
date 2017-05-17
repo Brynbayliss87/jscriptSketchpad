5052908bd232fca5eae64788bc40c5af9da18729
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-17 19:31:56 +0000

    Started function to change grid size

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 
 
-$(document).ready(function(){
-  var size = 16
-  
-  
-  
+$(document).ready(function(){ 
    
-  var make_wrapper = function(size){
+  var makeWrapper = function(size){
   for(var i=0; i < size; i++){
     for(var j=0; j < size; j++){ 
-      var square  = $("<div class='square'></div>");
-      
-  
+      var square  = $("<div class='square'></div>");     
       square.appendTo("#wrapper");
-      $("square").css("height","640/size");
-      $("square").css("height","640/size");
         
     }
   }
  }
-  
-  make_wrapper(size)
 
-  $("button").click(function(){
-    size = prompt("Set the size, pick a number between 1 and 64")
-    $("square").remove();
-    make_wrapper(size);
-  });
+var size = 16
   
+makeWrapper(size)
 
+  
+var squareSize = $(".square").outerHeight();
+
+var resizeGrid = function(newSize){
+  $(".square").remove()
+  makeWrapper(newSize);
+  $(".square").outerHeight(640/newSize);
+}
+
+$("button").click(function(){
+  size = prompt("Set the size, pick a number between 1 and 64")
+  resizeGrid(size);
+});
 
 });
+
 
 
