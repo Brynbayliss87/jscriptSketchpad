179a652f97715af96987cf12ae025829d3269ddc
Bryn Bayliss <brynbayliss@hotmail.com>
2017-05-21 19:00:41 +0000

    Fixed grid borders when square size changed

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 
 
-$(document).ready(function(){ 
+$(document).ready(function(){
+  var size = 16;
+  makeWrapper(size);
+  resizeGrid(size);
+}); 
    
   var makeWrapper = function(size){
-  for(var i=0; i < size; i++){
-    for(var j=0; j < size; j++){ 
-      var square  = $("<div class='square'></div>");     
-      square.appendTo("#wrapper");
+  for(var i=0; i < size*size; i++){
+    var square  = $("<div class='square'></div>");     
+    square.appendTo("#wrapper");
         
-    }
-  }
- }
+    };}
+  $(".square").on("mouseenter",function(){
+    $(this).addClass("trail");
+  });
+  
 
 var size = 16
   
 makeWrapper(size)
 
   
-var squareSize = $(".square").outerHeight();
+
 
 var resizeGrid = function(newSize){
   $(".square").remove()
   makeWrapper(newSize);
-  $(".square").outerHeight(640/newSize);
-  $(".square").outerWidth(640/newSize);
-}
+  var squareSize = 640/(newSize*1.1);
+  $(".square").css({
+  "width":squareSize+"px",
+  "height":squareSize+"px",
+  "border-width":squareSize*0.05+"px",
+  })
 
-$("button").click(function(){
+
+  $("button").click(function(){
   size = prompt("Set the size, pick a number between 1 and 64")
   resizeGrid(size);
-});
+  makeWrapper(size);
+  });
+};
 
-});
 
 
 
